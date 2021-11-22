7aafd31c760c55bdad87167aa366d13afe79ee85
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2021-11-22 16:24:39 +0900

    add constrained least square solver

--- manifold projection/ManifoldProjection.py
+++ manifold projection/ManifoldProjection.py
@@ -1,6 +1,7 @@
+import numpy as np
 from copy import deepcopy
 
-class KNN: # O(nlogn) implementation, should be revised to O(n) using nth_element
+class KNN:
     def __init__(self, v_data):
         """
         @param v_data   list of feature vector(= 512 dim)
@@ -15,13 +16,18 @@ class KNN: # O(nlogn) implementation, should be revised to O(n) using nth_elemen
         """
         return sum((v_1 - v_2) ** 2) ** 0.5
 
-    def __call__(self, v_in, k = 10):
+    def __call__(self, v_in, k = 10): # must be len(self.v_data) >= 10
         """
         @param v_in     input feature vector(= 512 dim)
         @return         k-nearest neighbor feature vector
         """
-        self.v_data.sort(key=lambda v: self.GetDist(v, v_in))
-        return deepcopy(self.v_data[:k])
+        # O(nlogn) implementation
+        # self.v_data.sort(key=lambda v: self.GetDist(v, v_in))
+        # return deepcopy(self.v_data[:k])
+
+        # O(n) implementation
+        ret = np.partition(self.v_data, k)
+        return ret[:k]
 
 class ConstrainedLeastSquareSolver:
     def __call__(self, v_in, v_k):
@@ -31,5 +37,17 @@ class ConstrainedLeastSquareSolver:
         @return
         """
         k = len(v_k)
-        # WIP
-        pass
+        C = np.zeros(shape=(k,k))
+        for i in range(k):
+            for j in range(k):
+                C[i][j] = np.dot((v_in - v_k[i]), (v_in - v_k[j]))
+        C_inv = np.linalg.inv(C)
+        w = np.zeros(shape=(k))
+        for i in range(k):
+            a = sum(C_inv[i][j] for j in range(k))
+            b = sum(C_inv[i][j] for i in range(k) for j in range(k))
+            w[i] = a / b
+        ret = np.zeros(shape=(512))
+        for i in range(k):
+            ret += w[i] * v_k[i]
+        return ret
