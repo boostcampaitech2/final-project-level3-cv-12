9e601e6de2071c67cd07726a230fe7bbe3f134d7
tkdlqh2 <yhj7124@naver.com>
2021-12-02 14:18:14 +0900

    Merge jinhan's

--- dataset.py
+++ dataset.py
@@ -1,122 +1,93 @@
-from torch.utils.data import Dataset
-import json
-import os
-import cv2
-from module_fold import CEModule
-import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import json, cv2, os
+from data_loader.aug import InitTransform
 
 
-class CustomDataset(Dataset):
-    def __init__(self, data_dir, part, mode, transform=None):
-        super().__init__()
-        self.data_dir = data_dir
-        self.part = part
-        self.mode = mode
-        self.transform = transform
+class BaseDataset(Dataset):
+    part_size = { 'left_eye': 128, 'right_eye': 128, 'nose': 168, 'mouth': 192, 'remainder': 512 }
 
-        patch_size = {"left_eye": 128,
-                      "right_eye": 128, "mouth": 192, "nose": 160, "face": 512}
-        self.patch_size = patch_size[part]
-        self.image_paths = []
-        self.points = []
+    def __init__(self, json_path):
+        self.info = json.load(open(json_path, 'r'))
+    
+    def __len__(self):
+        return len(self.info)
+    
+    def __getitem__(self, idx):
+        raise NotImplementedError
 
-        self.setup(part, mode)
+    def _get_center(self, idx, part):
+        x, y = self.info[str(idx)][part]
+        x, y = int(x), int(y)
+        sz   = self.part_size[part] // 2
+        if x - sz < 0:   x = sz
+        if x + sz > 512: x = 512 - sz
+        if y - sz < 0:   y = sz
+        if y + sz > 512: y = 512 - sz
+        return x, y
 
-    def __getitem__(self, index):
 
-        image = self.read_image(index)
-        point = self.points[index]
+class CEDataset(BaseDataset):
+    def __init__(self, json_path, part, transform=None):
+        super().__init__(json_path)
+        self.part = part # 'left_eye', 'right_eye', 'nose', 'mouth', 'remainder'
+        # self.init_transform = InitTransform(512)
+        self.transform      = transform
 
-        if self.part != "face":
-            xmin = point[0]-(self.patch_size)//2
-            xmax = point[0] + (self.patch_size)//2
-            ymin = point[1]-(self.patch_size)//2
-            ymax = point[1]+(self.patch_size)//2
+    def __getitem__(self, idx):
+        # load image
+        img_path = self.info[str(idx)]['sketch_path']
+        img      = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE).astype(float)
+        # img      = self.init_transform(img)
+        img      = img / 255
 
-            if xmin < 0:
-                xmin, xmax = 0, self.patch_size
-            if xmax > 512:
-                xmin, xmax = 512 - self.patch_size, 512
-            if ymin < 0:
-                ymin, ymax = 0, self.patch_size
-            if ymax > 512:
-                ymin, ymax = 512 - self.patch_size, 512
-
-            patch_image = image[ymin:ymax, xmin:xmax]
-            patch_image_trans = patch_image
-
+        # crop
+        if self.part != 'remainder':
+            x, y = self._get_center(idx, self.part)
+            sz   = self.part_size[self.part] // 2
+            img  = img[y-sz:y+sz, x-sz:x+sz]
         else:
-            image[point[0][1]-64:point[0][1]+64,
-                  point[0][0]-64:point[0][0]+64] = 1
-            image[point[1][1]-64:point[1][1]+64,
-                  point[1][0]-64:point[1][0]+64] = 1
-            image[point[2][1]-96:point[2][1]+96,
-                  point[2][0]-96:point[2][0]+96] = 1
-            image[point[3][1]-80:point[3][1]+80,
-                  point[3][0]-80:point[3][0]+80] = 1
+            x1, y1, sz1 = self._get_center('left_eye'), self.part_size['left_eye'] // 2
+            x2, y2, sz2 = self._get_center('right_eye'), self.part_size['right_eye'] // 2
+            x3, y3, sz3 = self._get_center('nose'), self.part_size['nose'] // 2
+            x4, y4, sz4 = self._get_center('mouth'), self.part_size['mouth'] // 2
+            img[y1-sz1:y1+sz1, x1-sz1:x1+sz1] = 1
+            img[y2-sz2:y2+sz2, x2-sz2:x2+sz2] = 1
+            img[y3-sz3:y3+sz3, x3-sz3:x3+sz3] = 1
+            img[y4-sz4:y4+sz4, x4-sz4:x4+sz4] = 1
 
-            patch_image = image
-            patch_image_trans = patch_image
-
+        # apply transform (Denoising AE)
+        img_trans = img
         if self.transform is not None:
-            patch_image_trans = self.transform(image=patch_image)
-            patch_image_trans = patch_image_trans["image"]
-
-        return patch_image, patch_image_trans
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def setup(self, part, mode):
-        json_path = os.path.join(self.data_dir, f"{mode}.json")
-        with open(json_path, "r") as f:
-            json_data = json.load(f)
-
-        for img in json_data:
-            image_path = os.path.join(self.data_dir, img)
-            if part != "face":
-                image_part_point = list(map(int, json_data[img][part]))
-            else:
-                image_part_point = []
-                image_part_point.append(
-                    list(map(int, json_data[img]["left_eye"])))
-                image_part_point.append(
-                    list(map(int, json_data[img]["right_eye"])))
-                image_part_point.append(
-                    list(map(int, json_data[img]["mouth"])))
-                image_part_point.append(list(map(int, json_data[img]["nose"])))
-
-            self.image_paths.append(image_path)
-            self.points.append(image_part_point)
-
-    def read_image(self, index):
-        image_path = self.image_paths[index]
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE).astype(float)
-        image /= 255
-        return image
+            img_trans = self.transform(img)
+        return img, img_trans
 
 
-class CustomVectorset(Dataset):
-    def __init__(self, img_dir, sketch_dir, pth_dir, mode):
-        super().__init__()
-        self.img_dir = img_dir
-        self.sketch_dir = sketch_dir
-        self.pth_dir = pth_dir
-        self.part = ['mouth', 'left_eye', 'right_eye', 'nose', 'face']
-        self.mode = mode
+class FEDataset(BaseDataset):
+    def __init__(self, json_path, fv_path):
+        super().__init__(json_path)
+        self.fv   = json.load(open(fv_path, 'r'))
+        self.init_transform = InitTransform(512)
+    
+    def __len__(self, ):
+        return len(self.info)
+    
+    def __getitem__(self, idx): # img(1), point(5), fv(5)
+        img_path = self.info[str(idx)]['image_path']
+        img      = cv2.imread(img_path, cv2.IMREAD_COLOR).astype(float)
+        img      = self.init_transform(img)
+        img      = img / 255
 
-    def __getitem__(self, index):
+        x1, y1 = self._get_center('left_eye')
+        x2, y2 = self._get_center('right_eye')
+        x3, y3 = self._get_center('nose')
+        x4, y4 = self._get_center('mouth')
+        point  = { 'left_eye': [x1, y1], 'right_eye': [x2, y2], 'nose': [x3, y3], 'mouth': [x4, y4] }
 
-        part_encoder = {}
+        fv1 = self.fv[str(idx)]['left_eye']
+        fv2 = self.fv[str(idx)]['right_eye']
+        fv3 = self.fv[str(idx)]['nose']
+        fv4 = self.fv[str(idx)]['mouth']
+        fv  = { 'left_eye': fv1, 'right_eye': fv2, 'nose': fv3, 'mouth': fv4 }
 
-        mouth_v = part_encoder["mouth"](mouth_patch)
-        l_eye_v = part_encoder["left_eye"](l_eye_patch)
-        r_eye_v = part_encoder["right_eye"](r_eye_patch)
-        nose_v = part_encoder["nose"](nose_patch)
-        face_v = part_encoder["face"](face_patch)
-
-        real_img =
-        return mouth_v, l_eye_v, r_eye_v, nose_v, face_v, real_img, points
-
-    def __len__(self):
-        return
+        return img, point, fv
