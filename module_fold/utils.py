56133e06de4357765da2fda895373695f71b2475
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2021-12-08 23:12:57 +0900

    modify weight_init

--- module_fold/utils.py
+++ module_fold/utils.py
@@ -4,18 +4,18 @@ import torch.nn as nn
 
 def weight_init_normal(m):
     classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
+    if 'Conv' in classname or 'Linear' in classname:
         nn.init.normal_(m.weight, 0.0, 0.02)
-    elif classname.find("Norm") != -1:
+    elif 'Norm' in classname:
         nn.init.normal_(m.weight, 1.0, 0.02)
         nn.init.constant_(m.bias, 0.0)
 
 
 def weight_init_kaiming(m):
     classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
+    if 'Conv' in classname or 'Linear' in classname:
         nn.init.kaiming_uniform_(m.weight)
-    elif classname.find("Norm") != -1:
+    elif 'Norm' in classname:
         nn.init.normal_(m.weight, 1.0, 0.02)
         nn.init.constant_(m.bias, 0.0)
  
