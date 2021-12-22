b9b9e13ff62af8b03a2b8877aa6fc624f6f33e39
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2021-12-23 02:30:29 +0900

    final

--- data/dockers/google_api_dir/init.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-python /home/myamya/project/remover.py
-python /home/myamya/project/rename.py 
-python /home/myamya/project/google_vision_api.py
-
--- data/image_files/delete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf ./cropped_512_image/*
-rm -rf ./cropped_image/*
-rm -rf ./sketched1/*
-rm -rf ./sketched2/*
