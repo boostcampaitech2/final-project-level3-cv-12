042110d72d7dac39cd3bc4e0c15d102850024d3d
T2232 <ssail@naver.com>
2021-12-23 07:22:17 +0000

    backend v2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,62 +1,68 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
+import os
+import cv2
+import numpy as np
+import torch
+import re
 import base64
+from typing import List, Union, Optional, Dict, Any
+from manifold.manifold import KNN, ConstrainedLeastSquareSolver
+from model.model import get_encoder, get_decoder, get_generator, get_knn, inference, save_image
+from fastapi import FastAPI, UploadFile, File, Body,Request
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from uuid import UUID, uuid4
-from typing import List, Union, Optional, Dict, Any
-import torch
-from datetime import datetime
-from model.model import get_encoder, get_decoder,get_knn,get_generator, inference
-from manifold.manifold import KNN, ConstrainedLeastSquareSolver
-from fastapi import Body
-import re
 
 app = FastAPI()
-
+loading=False
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins=["*"],
-	allow_credentials=True,
-	allow_methods=["*"],
-	allow_headers=["*"],
+   CORSMiddleware,
+   allow_origins=["*"],
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
 )
 
 @app.on_event('startup')
 def load_models():
-	encoder_file_path  = '/opt/ml/backend/project/_DB/encoder_all_260_1216_1424.pth'
-	decoder_file_path  = '/opt/ml/backend/project/_DB/decoder_all_260_1216_1424.pth'
-	generator_pth_path = '/opt/ml/backend/project/_DB/generator/generator_tj.pth'
-	fv_json_path       = '/opt/ml/backend/project/_DB/fv_all_train.json'
+   encoder_path   = '/opt/ml/backend/model_save/encoder_all_260_1216_1424.pth'
+   decoder_path   = '/opt/ml/backend/model_save/decoder_all_260_1216_1424.pth'
+   generator_path = '/opt/ml/backend/model_save/pix2pixU.pth'
+   fv_json_path   = '/opt/ml/backend/model_save/fv_all_train.json'
 
-	use_cuda = torch.cuda.is_available()
-	global device
-	device = torch.device('cuda' if use_cuda else 'cpu')
-	global encoder
-	encoder = get_encoder(encoder_file_path, 'all', device)
-	global decoder
-	decoder = get_decoder(decoder_file_path, 'all', device)
-	global generator
-	generator = get_generator(generator_pth_path, device)
-	global knn
-	knn = get_knn(fv_json_path)
-	encoder.to(device)
-	decoder.to(device)
-	generator.to(device)
+   global device, encoder, decoder, generator, knn, least_square
+   use_cuda = torch.cuda.is_available()
+   device = torch.device('cuda' if use_cuda else 'cpu')
+   encoder = get_encoder(encoder_path, 'all', device)
+   decoder = get_decoder(decoder_path, 'all', device)
+   generator = get_generator(generator_path, device)
+   knn = get_knn(fv_json_path)
+   least_square = ConstrainedLeastSquareSolver()
 
 @app.get("/")
 def hello_world():
-	return {"hello": "world"}
+   return {"hello": "world"}
 
 @app.post("/")
-async def test(data: str = Body(...)):
-	imgstring = re.sub('^data:image/.+;base64,', '', data)
-	imgdata = base64.b64decode(imgstring)
-	filename = 'some_image.png'  # I assume you have a way of picking unique filenames
-	with open(filename, 'wb') as file:
-		file.write(imgdata)
-	least_square = ConstrainedLeastSquareSolver()
-	t = 0.4
-	k = 10
-	inference(filename, encoder,decoder,generator,knn,least_square, device, t, k)
-	return {"hello": "world"}
+async def test(request: Request):
+   loading = True
+   item = await request.json()
+   data = item['image']
+   imgstring = re.sub('^data:image/.+;base64,', '', data)
+   imgdata = base64.b64decode(imgstring)
+   filename = 'some_image.png'  # I assume you have a way of picking unique filenames
+   with open(filename, 'wb') as file:
+      file.write(imgdata)
+   
+   t = item['T']
+   k = 10
+   sketch_img = 1 - cv2.imread(filename, cv2.IMREAD_GRAYSCALE).astype(float) / 255
+   inferenced_img = inference(sketch_img, encoder, decoder, generator, knn, least_square, device, t, k)
+   save_image(inferenced_img, 'output.png')
+   return {"hello": "world"}
 
+@app.get("/output.png")
+def image():
+   while (loading):
+      sleep(0.05)
+   return FileResponse("./output.png")
--- backend/model/model.py
+++ backend/model/model.py
@@ -18,6 +18,8 @@ from manifold.manifold import KNN, ConstrainedLeastSquareSolver
 # -- basic function
 # ==========================================================
 
+part = ['left_eye', 'right_eye', 'nose', 'mouth', 'remainder']
+
 def seed_everything(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -53,8 +55,8 @@ def get_decoder(decoder_path, part, device):
 
 
 def get_generator(generator_path, device):
-    generator = ISModule.Generator(input_nc=1, output_nc=3, ngf=56, n_downsampling=3,
-                                   n_blocks=9, norm_layer=nn.BatchNorm2d, padding_type='reflect')
+    generator = ISModule.Generator_U(input_nc=5, output_nc=3, ngf=56,
+                                     n_blocks=9, norm_layer=nn.BatchNorm2d, padding_type='reflect')
     checkpoint = torch.load(generator_path)
     state_dict = checkpoint.state_dict()
     generator.load_state_dict(state_dict)
@@ -90,56 +92,20 @@ def save_image(img, img_path):
 # -- main function
 # ==========================================================
 
+pos = [(256, 256), (302, 256), (244, 326), (244, 186), (385, 256)]
+sz  = [512, 160, 128, 128, 128]
+
 def inference(img, encoder, decoder, generator, knn, least_square, device, t, k):
-    img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     img = torch.FloatTensor(img).to(device)
     img = img.unsqueeze(axis=0)
     img = img.unsqueeze(axis=1)
     fv = encoder(img).to(device)
     fv = fv_proj(fv, knn, least_square, device, t, k)
-    sketch = decoder(fv)
+    img = decoder(fv)
+
+    sketch = torch.FloatTensor(np.zeros((1, 5, 512, 512))).to(device)
+    img_patch = [img[:, :, pos[i][0]-sz[i]//2:pos[i][0]+sz[i]//2, pos[i][1]-sz[i]//2:pos[i][1]+sz[i]//2].clone() for i in range(5)]
+    for i in range(1, 5): img_patch[0][:, :, pos[i][0]-sz[i]//2:pos[i][0]+sz[i]//2, pos[i][1]-sz[i]//2:pos[i][1]+sz[i]//2] = 0
+    for i in range(5): sketch[:, i, pos[i][0]-sz[i]//2:pos[i][0]+sz[i]//2, pos[i][1]-sz[i]//2:pos[i][1]+sz[i]//2] = img_patch[i]
     output = generator(sketch)
-    save_image(output, "./output.png")
-
-
-def test(encoder_path, decoder_path, generator_path, fv_json_path, t, k):
-    # -- setting
-    seed_everything(42)
-    use_cuda = torch.cuda.is_available()
-    device   = torch.device('cuda' if use_cuda else 'cpu')
-
-    # -- encoder, decoder, generator, knn, least_square
-    encoder = get_encoder(encoder_path, 'all', device)
-    decoder = get_decoder(decoder_path, 'all', device)
-    generator = get_generator(generator_path, device)
-    knn = get_knn(fv_json_path)
-    least_square = ConstrainedLeastSquareSolver()
-
-    # -- img_path
-    save_img_path = f'/opt/ml/project/inference_save/{get_time()}_all'
-    load_img_path = '/opt/ml/project/inference_save/test'
-    os.makedirs(save_img_path, exist_ok=True)
-    print(save_img_path)
-
-    for img_name in tqdm(sorted(os.listdir(load_img_path))):
-        img_path = os.path.join(load_img_path, img_name)
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE).astype(float)
-        img = 1 - img / 255
-        output, sketch = inference(img, encoder, decoder, generator, knn, least_square, device, t, k)
-        save_image(output, os.path.join(save_img_path, 'output_' + img_name))
-        save_image(1 - sketch, os.path.join(save_img_path, 'sketch_' + img_name))
-
-
-if __name__ == '__main__':
-    # -- file_path: left_eye, right_eye, nose, mouth, remainder
-    encoder_path = '/opt/ml/project/model_save/encoder_all_260_1216_1424.pth'
-    decoder_path = '/opt/ml/project/model_save/decoder_all_260_1216_1424.pth'
-    generator_path = '/opt/ml/project/_DB/generator/generator_tj.pth'
-    fv_json_path = '/opt/ml/project/data/fv_all_train.json'
-
-    # -- parameter
-    t = 0.4
-    k = 5
-
-    # -- inference
-    test(encoder_path, decoder_path, generator_path, fv_json_path, t, k)
+    return output
